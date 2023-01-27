257b6144f5ce8a0be836aab66d788dc2d228ddb7
Maria Eduarda <80916152+Mariachf@users.noreply.github.com>
2023-01-27 19:44:52 -0300

    atividade-console

---
+++ script.js
@@ -0,0 +1,56 @@
+1
+
+/* function bemVindo(){
+    console.log("Seja Bem Vindo")
+}
+
+bemVindo() */
+
+2
+
+/* function Nome(nome){
+
+    console.log('Meu nome é ' +  nome)
+}
+
+Nome('Maria') */
+
+3
+
+/* function infos(nome,idade,estiloMusical){
+    return nome + idade + estiloMusical
+  }
+  
+  const informação = infos("Meu nome é Maria ","tenho 23 anos ","e gosto de MPB.")
+  
+  console.log(informação) */
+
+4
+
+/*   const info = function(filme,musica){
+    console.log(filme);
+    console.log(musica);
+}
+   info("avatar", "é o amor");
+   */
+
+   5
+
+/*    function triplo(num){
+    return num*3
+   }
+
+   console.log(triplo(3)) */
+
+   6
+
+/* let hoje = true
+let isTrue ="hoje é dia 30"
+
+let isFalse ="hoje não é dia 27"
+
+if (hoje == false){
+console.log(isTrue)
+} else{
+console.log(isFalse)
+} */
